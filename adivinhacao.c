197b2d2824908d68154ad32a8551ef301c90d140
Philippe Lima <42221580+philippe-piu@users.noreply.github.com>
2023-03-13 14:50:20 -0300

    Jogo-de-Adivinhacao

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,39 +1,50 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
 
 //Jogo de Adivinhação
 int main()
 {
-	//Número secreto definido para a máquina
-	int numerosecreto = 42;
-	int chuteusuario = 0;
-	int acertou = 0;
-	int maior = 0;
-	int tentativas = 1;
-	
+	int numerosecreto, chuteusuario, acertou, maior, tentativas, numerograndesecreto, segundos;
+	double pontos;
+	//gerando número secreto diferentes por numero de segundos
+	segundos = time(0);
+	srand(segundos);
+	//gerando um número secreto randomico
+	numerograndesecreto = rand();
+	numerosecreto = numerograndesecreto % 100;
+	chuteusuario = 0;
+	acertou = 0;
+	maior = 0;
+	tentativas = 1;
+	pontos = 1000;
+
 	//Imprime o cabeçalho do nosso jogo seguindo as aulas da Alura;
 	printf("******************************************\n");
 	printf("* Bem vindo ao nosso jogo de adivinhacao *\n");
 	printf("******************************************\n");
 	printf("\n***********************************************************************************");
-	
+
 
 	//Loop de repetição
-	while(1){
+	while(1)
+	{
 		printf("\n*****************************************************************************\n");
 		printf("*Tentativa %d  *\n", tentativas);
 		printf("*Qual e o seu chute para descobrir o numero secreto escondido pela maquina? *");
 		printf("\n*****************************************************************************\n");
 		scanf("%d", &chuteusuario);
-		
+
 		//Condição para o usuário não pode colocar números negaivos
-		if(chuteusuario < 0){
+		if(chuteusuario < 0)
+		{
 			printf("\n*********************************************************\n");
 			printf("*Caro usuario voce nao pode informar numeros negativos !*\n");
 			printf("*********************************************************\n");
 			//Continue para execução desse bloco de código e e faz voltar a funcionar e vai para proxima interação do loop
 			continue;
 		}
-		
+
 		//Mostra o valor informado pelo o usuário
 		printf("\n******************************************\n");
 		printf("*Numero informado pelo usuario e %d      *\n", chuteusuario);
@@ -63,15 +74,22 @@ int main()
 			printf("*Seu chute foi menor que o numero secreto tente novamente*\n");
 			printf("**********************************************************\n");
 		}
-		
+
 		//Conta o numero de tentativas do usuário
 		tentativas ++;
+		//Calculo dos pontos do jogo
+		//variaveis do tipo inteira (chuteusuario - numerosecreto) e 2.0 double   
+		//double pontosperdidos = abs(chuteusuario - numerosecreto) / (double)2; seu eu tivesse colocado isso dava certo também.                
+		//abs passa o número para absoluto  de negativo para positivo
+		double pontosperdidos = abs(chuteusuario - numerosecreto) / 2.0;
+		pontos = pontos - pontosperdidos;
 	}
-	
-	printf("\n********************************\n");
-	printf("*Fim do Jogo !                 *\n");
-	printf("**Voce acertou em %d tentativas**\n",tentativas);
-	printf("********************************\n");
+
+	printf("\n******************************************\n");
+	printf("*           Fim de Jogo !                *\n");
+	printf("*****  Voce acertou em %d tentativas  *****\n", tentativas);
+	printf("*Total de pontos feitos %.1lf            *\n", pontos);
+	printf("******************************************\n");
 
 
 	return(0);
